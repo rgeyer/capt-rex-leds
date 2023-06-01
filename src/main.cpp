2d4de438a65cec8e959662c8e88789ce23eaec33
Ryan J. Geyer <me@ryangeyer.com>
2023-06-01 12:50:11 -0700

    Add a stop/start button

--- src/main.cpp
+++ src/main.cpp
@@ -2,62 +2,76 @@
 #include <Adafruit_NeoPixel.h>
 
 #define NEO_PIXEL_PIN PD7
+#define BUTTON_PIN PD6
 
 #define INCREMENT 30
 
-class Bar {
-    private:
-    int intensities[3];
-    bool direction;
-    int gap;
-    int steps;
-    
-    public:
-    Bar() {}
-    
-    void init(int starting_intensity, int g) {
-        gap = g;
-        steps = 0;
-        for (int i=0; i <= 2; i++) {
-            intensities[i] = 0;
-        }
+class Bar
+{
+private:
+  int intensities[3];
+  bool direction;
+  int gap;
+  int steps;
+
+public:
+  Bar() {}
+
+  void init(int starting_intensity, int g)
+  {
+    gap = g;
+    steps = 0;
+    for (int i = 0; i <= 2; i++)
+    {
+      intensities[i] = 0;
     }
-    
-    void update() {
-        if (intensities[0] >= 255) {
-            // The sweep has reached full intensity for all LEDs, time to count down.
-            direction = false;
-            steps = 0;
-        }
-            
-        if (intensities[2] <= 0) {
-            // The sweep has reached zero intensity for all LEDs, time to count up.
-            direction = true;
-            steps = 0;
-        }
-        steps++;
-        if (direction) {
-            int last_intensity = intensities[0];
-            for (int i=0; i <= 2; i++) {
-                last_intensity = last_intensity + gap;
-                intensities[i] = (last_intensity > 255) ? 255 : last_intensity;
-            }
-        } else {
-            int last_intensity = intensities[2];
-            for (int i=2; i >= 0; i--) {
-                last_intensity = last_intensity - gap;
-                intensities[i] = (last_intensity < 0) ? 0 : last_intensity;
-            }
-        }
+  }
+
+  void update()
+  {
+    if (intensities[0] >= 255)
+    {
+      // The sweep has reached full intensity for all LEDs, time to count down.
+      direction = false;
+      steps = 0;
     }
 
-    int getPixel(int i) {
-      return intensities[i];
+    if (intensities[2] <= 0)
+    {
+      // The sweep has reached zero intensity for all LEDs, time to count up.
+      direction = true;
+      steps = 0;
     }
+    steps++;
+    if (direction)
+    {
+      int last_intensity = intensities[0];
+      for (int i = 0; i <= 2; i++)
+      {
+        last_intensity = last_intensity + gap;
+        intensities[i] = (last_intensity > 255) ? 255 : last_intensity;
+      }
+    }
+    else
+    {
+      int last_intensity = intensities[2];
+      for (int i = 2; i >= 0; i--)
+      {
+        last_intensity = last_intensity - gap;
+        intensities[i] = (last_intensity < 0) ? 0 : last_intensity;
+      }
+    }
+  }
+
+  int getPixel(int i)
+  {
+    return intensities[i];
+  }
 };
 
 Adafruit_NeoPixel strip;
 Bar bar;
+bool run = false;
 
 void setup()
 {
@@ -66,19 +80,30 @@ void setup()
   strip.clear();
   strip.show();
   bar.init(0, INCREMENT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop()
 {
-  strip.clear();
-  bar.update();
-  for (int i = 0; i <= 2; i++)
+  if (run)
   {
-    // For red
-    strip.setPixelColor(i, strip.Color(bar.getPixel(i), 0, 0));
-    // For blue
-    // strip.setPixelColor(i, strip.Color(0, 0, bar.getPixel(i)));
+    strip.clear();
+    bar.update();
+    for (int i = 0; i <= 2; i++)
+    {
+      // For red
+      strip.setPixelColor(i, strip.Color(bar.getPixel(i), 0, 0));
+      // For blue
+      // strip.setPixelColor(i, strip.Color(0, 0, bar.getPixel(i)));
+    }
+    strip.show();
   }
-  strip.show();
+  if (digitalRead(BUTTON_PIN) == LOW)
+  {
+    run = !run;
+    strip.clear();
+    strip.show();
+    delay(500);
+  }
   delay(100);
 }
